9997faf0e64f7ed310285bb8039168ca95e63231
t.shulgin <t.shulgin@annasoft.ru>
2020-11-03 14:01:20 +0300

    Added mysql

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime, timedelta
+
 
 # Create your models here.
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=50)
+    blog_date = models.DateTimeField(default=datetime.now()+timedelta(days=30))
+    blog_text = models.TextField(max_length=1500)
+    blog_image = models.ImageField(upload_to='blog_images/')
+
+
--- newbie_blog/settings.py
+++ newbie_blog/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'newbie_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# Original database Backup settings
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newbie_blog_db',
+        'USER': 'postgres',
+        'PASSWORD': '1qazXSW@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -123,4 +135,4 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/img_media/'
